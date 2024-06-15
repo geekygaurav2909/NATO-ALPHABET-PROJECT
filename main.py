2035a7f4a072140e62bbf85c678e4761f1d680e5
geekygaurav2909 <168617705+geekygaurav2909@users.noreply.github.com>
2024-06-15 13:16:12 +0530

    Added exception handling part

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
 import pandas as pd
-student_data_frame = pd.DataFrame(student_dict)
+# student_data_frame = pd.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
+is_valid_input =  False
 
 nato_file = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for index, row in nato_file.iterrows()}
 # print(nato_dict.values())
+# print(nato_dict)
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter the name: ").upper()
+while not is_valid_input:
+    user_input = input("Enter the name: ").upper()
 
-phonetic_list = [nato_dict[letter] for letter in user_input]
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_input]
+        is_valid_input = True
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_list)
 
-print(phonetic_list)
 
